040d103bd3a118339223c5cab4de38e67f52f47a
Dashinja <byron@dashinja.com>
2024-04-20 02:42:40 -0400

    Cleanup of calendarAlarmService

--- calarmhelp/services/calendarAlarmService.py
+++ calarmhelp/services/calendarAlarmService.py
@@ -48,14 +48,6 @@ class CalendarAlarmService:
 
     def create_alarm_json(self, user_input: str):
 
-        # unused = """Your response will be like this: {
-        #                     name: string,
-        #                     category: always | work | home,
-        #                     lead_time: number,
-        #                     event_time: datetime
-        #                     creation_time: currentDateTime
-        #                 }"""
-        #
         systemPrompt = ("system", """You respond with JSON only. From the user_input you will extract the name (string) of the event, the category of one of the types "always | work | home" for the event (if category is unclear, default to "always"), lead_time (number) which is how long before the event I should be reminded, event_time (datetime) is when the event should actually happen, and creation_time of the call to the llm (currentDateTime) Your JSON response will only have the fields 'name', 'category', 'lead_time' (always expressed in terms of minutes. So 3 hours = 180 for example), 'event_time', and 'creation_time'.
                         {user_input}
                         {current_time}
@@ -76,32 +68,14 @@ class CalendarAlarmService:
         tools = [createCalendarReadout]
         print("*=*=*=*=*=*=|||||||||||||||||Created Tools*=*=*=*=*=*=|||||||||||||||||")
 
-        # agent: Runnable[BaseSingleActionAgent, BaseMultiActionAgent] = create_openai_tools_agent(llm, tools, prompt)
-
         agent = create_openai_tools_agent(llm, tools, prompt)
         print("*=*=*=*=*=*=|||||||||||||||||Created AGENT*=*=*=*=*=*=|||||||||||||||||")
 
-        # agentResponse2 = agent.invoke({
-        #     "user_input": user_input,
-        #     "current_time": datetime.datetime.now().isoformat()
-        # })
         print("*=*=*=*=*=*=|||||||||||||||||INVOKED agent to get agentResponse2*=*=*=*=*=*=|||||||||||||||||")
 
         agent_executer = AgentExecutor(agent=agent, tools=tools, verbose=True)  # type: ignore
         print("*=*=*=*=*=*=|||||||||||||||||Agent executer created*=*=*=*=*=*=|||||||||||||||||")
 
-        # chain = prompt | llm
-        # agentResponse = agent_executer.invoke({
-        #     "user_input": user_input,
-        #     # The current time gives LLM context of 'now' and helps it understand dates for words like "tomorrow" etc.
-        #     "current_time": datetime.datetime.now().isoformat()
-        # })
-
-        # response = chain.invoke({
-        #     "user_input": user_input.input,
-        #     # The current time gives LLM context of 'now' and helps it understand dates for words like "tomorrow" etc.
-        #     "current_time": datetime.datetime.now().isoformat()
-        # })
         print("*=*=*=*=*=*=|||||||||||||||||BEFORE BEFORE response variable populated*=*=*=*=*=*=|||||||||||||||||")
         response = agent_executer.invoke({
             "user_input": user_input,
