158cc7bc9fe49fcebd77f8063db0efb096b3e1a1
Dashinja <byron@dashinja.com>
2024-08-23 01:04:08 -0400

    just a save point | needs redo

--- calarmhelp/services/util/llmSetup.py
+++ calarmhelp/services/util/llmSetup.py
@@ -5,7 +5,7 @@ from langchain.callbacks import AsyncIteratorCallbackHandler
 load_dotenv()
 
 # Initialize the ChatBot using the 'gpt-4-turbo' model with a temperature of 0
-ChatBot = ChatOpenAI(model='gpt-4-turbo', temperature=0)
+ChatBot = ChatOpenAI(model='gpt-4o', temperature=0)
 
 # Create a callback handler for asynchronous iteration
 callback = AsyncIteratorCallbackHandler()
